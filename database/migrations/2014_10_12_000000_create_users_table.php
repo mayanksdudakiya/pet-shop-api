bb3c10e0b9a95f2658e14d00d78c96c875152ee8
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-13 02:46:31 +0530

    chore: unique index added in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,7 +9,7 @@ return new class () extends Migration {
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->uuid('uuid');
+            $table->uuid('uuid')->unique();
             $table->string('first_name');
             $table->string('last_name');
             $table->boolean('is_admin')->default(false);
