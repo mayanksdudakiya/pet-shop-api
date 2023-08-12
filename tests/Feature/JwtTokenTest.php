435c208b20373deaf76145177b9446069cb699a8
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-13 00:54:35 +0530

    test: jwt token valid expiration time test added

--- tests/Feature/JwtTokenTest.php
+++ tests/Feature/JwtTokenTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use App\Facades\JwtAuth;
 use App\Models\User;
+use Carbon\Carbon;
+use DateTimeImmutable;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Lcobucci\JWT\Token;
@@ -23,5 +25,21 @@ class JwtTokenTest extends TestCase
         $this->assertInstanceOf(Token::class, $token);
         $this->assertSame($user->uuid, $token->claims()->get('user_uuid'));
         $this->assertSame($user->type(), $token->claims()->get('access_level'));
+    }
+
+    /** @test */
+    public function jwt_token_should_have_valid_expiration_time(): void
+    {
+        $user = User::factory()->create();
+        $token = JwtAuth::issueToken($user);
+        $now = new DateTimeImmutable();
+
+        $currentTimeInUtc = Carbon::parse($now);
+        $expirationTimeInUtc = Carbon::parse($token->claims()->get('exp'));
+
+        $timeDifferenceInMinutes = $currentTimeInUtc->diffInMinutes($expirationTimeInUtc);
+
+        $this->assertFalse($token->isExpired($now));
+        $this->assertGreaterThanOrEqual($timeDifferenceInMinutes, 4);
     }
 }
