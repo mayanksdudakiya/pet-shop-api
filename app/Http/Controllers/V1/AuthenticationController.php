e126b025427be7a5272daec9f8655d1a138448b9
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-12 15:45:59 +0530

    feat: Intial Auth controller added

---
+++ app/Http/Controllers/V1/AuthenticationController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthenticationController extends Controller
+{
+    public function login()
+    {
+
+    }
+}
