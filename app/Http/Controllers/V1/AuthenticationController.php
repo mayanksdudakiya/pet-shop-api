13e2e11dcdcef2f91569e2a9b4b607d794b12173
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-13 03:33:55 +0530

    chore: JWT toekn stored in login

--- app/Http/Controllers/V1/AuthenticationController.php
+++ app/Http/Controllers/V1/AuthenticationController.php
@@ -6,22 +6,40 @@ use App\Facades\ApiResponse;
 use App\Facades\JwtAuth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\LoginRequest;
+use App\Models\JwtToken;
 use App\Models\User;
+use ErrorException;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class AuthenticationController extends Controller
 {
-    public function login(LoginRequest $request)
+    public function login(LoginRequest $request): JsonResponse
     {
-        $user = User::whereEmail($request->email)->firstOrFail();
+        try {
+            DB::beginTransaction();
 
-        throw_unless($user->password && Hash::check($request->password, $user->password), new AuthenticationException());
+            $user = User::whereEmail($request->email)->firstOrFail();
 
-        $token = JwtAuth::issueToken($user);
+            throw_unless($user->password && Hash::check($request->password, $user->password), new AuthenticationException());
 
-        return ApiResponse::sendSuccess([
-            'token' => $token->toString(),
-        ]);
+            $token = JwtAuth::issueToken($user);
+
+            JwtToken::storeToken($user, $token);
+
+            DB::commit();
+
+            return ApiResponse::sendSuccess([
+                'token' => $token->toString(),
+            ]);
+        } catch (Throwable $e) {
+            DB::rollBack();
+            Log::error($e->getMessage());
+            throw new ErrorException();
+        }
     }
 }
