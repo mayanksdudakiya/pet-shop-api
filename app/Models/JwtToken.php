edc527b62fa793a521d2dc646f4912652ea96463
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-13 03:44:17 +0530

    ref: added getTokenByUniqueId method

--- app/Models/JwtToken.php
+++ app/Models/JwtToken.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Lcobucci\JWT\UnencryptedToken;
 
 class JwtToken extends Model
 {
@@ -31,4 +32,18 @@ class JwtToken extends Model
         'expires_at' => 'datetime',
         'refreshed_at' => 'datetime',
     ];
+
+    public static function storeToken(User $user, UnencryptedToken $token): void
+    {
+        $user->tokens()->create([
+            'unique_id' => $token->claims()->get('jti'),
+            'token_title' => 'authentication',
+            'expires_at' => $token->claims()->get('exp'),
+        ]);
+    }
+
+    public static function getTokenByUniqueId(UnencryptedToken $token): ?self
+    {
+        return self::whereUniqueId($token->claims()->get('jti'))->first();
+    }
 }
