de9b61320048b85aa95c2f110fc2ca985d3ab59b
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-13 00:23:17 +0530

    test: password required & max len test added

--- tests/Feature/AuthenticationTest.php
+++ tests/Feature/AuthenticationTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Database\Seeders\DatabaseSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 use Illuminate\Support\Str;
@@ -57,6 +58,40 @@ class AuthenticationTest extends TestCase
         $response->assertJsonStructure([
             'errors' => [
                 'email'
+            ]
+        ]);
+    }
+
+    /** @test */
+    public function login_password_should_be_required(): void
+    {
+        $response = $this->postJson(route('api.admin.login'), [
+            'email' => DatabaseSeeder::ADMIN_EMAIL,
+            'password' => '',
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'errors' => [
+                'password'
+            ]
+        ]);
+    }
+
+    /** @test */
+    public function login_password_length_should_be_validated(): void
+    {
+        $response = $this->postJson(route('api.admin.login'), [
+            'email' => DatabaseSeeder::ADMIN_EMAIL,
+            'password' => Str::random(51),
+        ]);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonStructure([
+            'errors' => [
+                'password'
             ]
         ]);
     }
