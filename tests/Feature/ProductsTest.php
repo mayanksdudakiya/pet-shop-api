ae66c5cae214a0c44d34f057ac00733f025e5c62
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-13 18:12:30 +0530

    test: product listing with paginated data

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -14,4 +14,28 @@ class ProductsTest extends TestCase
     {
         $this->getJson(route('api.products'))->assertOk();
     }
+
+    /** @test */
+    public function products_can_be_listed_with_paginated_data(): void
+    {
+        Product::factory(15)->create();
+
+        $this->getJson(route('api.products'))
+            ->assertJsonStructure([
+                'current_page',
+                'data' => [
+                    'category_uuid',
+                    'title',
+                    'uuid',
+                    'price',
+                    'metadata',
+                    'category',
+                    'brand',
+                ]
+            ])
+            ->assertJsonCount(10, 'data')
+            ->assertJsonPath('current_page', 1)
+            ->assertJsonPath('total', 15)
+            ->assertJsonPath('last_page', 2);
+    }
 }
