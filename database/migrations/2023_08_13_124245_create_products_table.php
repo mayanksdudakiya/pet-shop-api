58bb411863f13588d7cc3aaf2f42f92bb9ce0446
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-13 18:28:09 +0530

    chore: products migration populated with columns

--- database/migrations/2023_08_13_124245_create_products_table.php
+++ database/migrations/2023_08_13_124245_create_products_table.php
@@ -4,22 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class () extends Migration {
     public function up(): void
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignUuid('category_uuid')->constrained('categories', 'uuid')->cascadeOnDelete();
+            $table->uuid('uuid')->unique();
+            $table->string('title');
+            $table->float('price');
+            $table->text('description');
+            $table->json('metadata');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('products');
