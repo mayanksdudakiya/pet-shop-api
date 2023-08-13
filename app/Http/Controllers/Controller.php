9f126e0c0949bf22e06d3f31c4294c8557e51b3a
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-14 02:39:03 +0530

    chore: swagger info & security schema added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,6 +6,22 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Pet Shop API",
+ *      description="This API has been created with the goal to test the coding skills of the candidates who are applying for a job position at Buckhill"
+ * )
+ *
+ * @OA\SecurityScheme(
+ *     type="http",
+ *     scheme="bearer",
+ *     securityScheme="bearerAuth",
+ *     description="Jwt Token Authentication",
+ *     name="Authorization",
+ *     in="header",
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
