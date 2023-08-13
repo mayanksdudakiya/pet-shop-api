7e6151da258b79aa9c1397bf5d43c1ea241b1f41
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-13 19:09:48 +0530

    chore: product model fillable & casts properties

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    /**
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'category_uuid',
+        'uuid',
+        'title',
+        'price',
+        'description',
+        'metadata',
+    ];
+
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'metadata' => 'json',
+    ];
 }
