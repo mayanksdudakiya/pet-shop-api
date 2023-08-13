a618188f30ef6404c873a2872a501b4b7944fd3b
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-14 02:37:33 +0530

    chore: admin auth swagger doc added

--- app/Http/Controllers/V1/AdminAuthController.php
+++ app/Http/Controllers/V1/AdminAuthController.php
@@ -8,13 +8,90 @@ use App\Http\Requests\V1\LoginRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Tag(
+ *     name="Admin",
+ *     description="Admin API endpoint"
+ * )
+ */
 class AdminAuthController extends Controller
 {
+    /**
+     * @OA\Post(
+     *     path="/api/v1/admin/login",
+     *     tags={"Admin"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *             mediaType="application/x-www-form-urlencoded",
+     *             @OA\Schema(
+     *                 @OA\Property(
+     *                     property="email",
+     *                     type="string",
+     *                     description="Admin email",
+     *                 ),
+     *                 @OA\Property(
+     *                     property="password",
+     *                     type="string",
+     *                     description="Admin password",
+     *                 ),
+     *                 required={"email", "password"}
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response="422",
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal server error"
+     *     )
+     * )
+     */
     public function login(LoginRequest $request): JsonResponse
     {
         return BaseAuth::login($request);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/v1/admin/logout",
+     *     tags={"Admin"},
+     *     @OA\Response(
+     *         response="200",
+     *         description="OK"
+     *     ),
+     *     @OA\Response(
+     *         response="401",
+     *         description="Unauthorized"
+     *     ),
+     *     @OA\Response(
+     *         response="404",
+     *         description="Page not found"
+     *     ),
+     *     @OA\Response(
+     *         response="422",
+     *         description="Unprocessable Entity"
+     *     ),
+     *     @OA\Response(
+     *         response="500",
+     *         description="Internal server error"
+     *     )
+     * )
+     */
     public function logout(Request $request): JsonResponse
     {
         return BaseAuth::logout($request);
