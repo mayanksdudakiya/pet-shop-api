f62da490a9a9846e908706041cef730ea7d8f199
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-14 00:28:11 +0530

    feat: product listing business logic added

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Facades\ApiResponse;
+use App\Http\Requests\V1\ListingRequest;
+use App\Http\Resources\V1\ProductResource;
+use App\Models\Product;
+use App\Traits\PaginateAndFilter;
 
 class ProductController extends Controller
 {
-    public function index()
-    {
+    use PaginateAndFilter;
 
+    public function index(ListingRequest $request)
+    {
+        $query = Product::query();
+
+        $this->applyFilters($query, $request);
+
+        $products = $this->paginateResults($query, $request);
+
+        $productCollection = ProductResource::collection($products);
+
+        return ApiResponse::sendData($productCollection);
     }
 }
