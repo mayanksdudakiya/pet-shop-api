82b7b37c696a36bb8790f2592911341bb52d93b2
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-13 21:27:59 +0530

    feat: listing paginate & filter trait added

---
+++ app/Traits/PaginateAndFilter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
+
+trait PaginateAndFilter
+{
+    protected function applyFilters(Builder $query, Request $request)
+    {
+        $sortOrder = $request->input('desc', false) ? 'desc' : 'asc';
+        $sortColumn = $request->input('sortBy', 'created_at');
+
+        return $query->orderBy($sortColumn, $sortOrder);
+    }
+
+    protected function paginateResults(Builder $query, Request $request): LengthAwarePaginator
+    {
+        $page = $request->input('page', 1);
+        $perPage = $request->input('limit', 10);
+
+        return $query->paginate(
+            $perPage = 15,
+            $columns = ['*'],
+            $pageName = 'page',
+            $page
+        );
+    }
+}
