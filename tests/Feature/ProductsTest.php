5fca86c4900d5654643e8bbfab7469c550c587ff
mayanksdudakiya <mayanksdudakiya@gmail.com>
2023-08-13 17:16:24 +0530

    test: Products endpoint test added

--- tests/Feature/ProductsTest.php
+++ tests/Feature/ProductsTest.php
@@ -3,10 +3,15 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ProductsTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    public function products_end_point_should_be_accessible(): void
+    {
+        $this->getJson(route('api.products'))->assertOk();
+    }
 }
